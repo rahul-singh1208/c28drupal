d818d4749a4ee4603b4bd42d0580ca601d85c3de
rahul-singh1208 <54431940+rahul-singh1208@users.noreply.github.com>
2021-06-21 16:40:47 +0530

    Update PageController.php
    Removed the query and coorectt he name space

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,14 +1,9 @@
 <?php
-
-/**
-@file
-Contains \Drupal\rahul_axelerant\Controller\PageController.
- */
 namespace Drupal\rahul_axelerant\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
-use \Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PageController extends ControllerBase
 {
@@ -18,20 +13,16 @@ class PageController extends ControllerBase
         $siteapi_key = \Drupal::config('rahul_axelerant.settings')->get('siteapi_key');
 
         if ($apikey == $siteapi_key) {
-            $query = \Drupal::entityQuery('node')
-                ->condition('nid', $nid)
-                ->condition('type', 'page', '=')
-                ->execute();
-
-            if (empty($query)) {
-                return new JsonResponse(array('error' => 'Invalid Node ID access denied'));
-            } else {
-                $node = Node::load($nid);
+            $node = Node::load($nid);
+            if (!empty($node) && ($node->getType()=='page')) {
                 $title = $node->getTitle();
                 $body = $node->get('body')->getValue()[0]['value'];
                 $response['title'] = $title;
                 $response['body'] = $body;
                 return new JsonResponse($response);
+
+            } else {
+                return new JsonResponse(array('error' => 'Invalid Node ID access denied'));
             }
 
         } else {
